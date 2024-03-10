e9dffb1d0808ad2a04558d1841dda396d4e9a293
Shakhzodbee <Bakhodirovichshakhzodbek@gmail.com>
2024-03-10 15:22:09 +0500

    tg notifications

---
+++ tg.messsenger.js
@@ -0,0 +1,5 @@
+let  eng =   ('Hello there our to-do list for today'),
+     uzb =   (`assalom aleykum  bugungi qilinadigan ishlarimiz ro'yxati `),
+     rus =   (`Привет наш список дел на сегодня`),
+    date =   (2024:10:03),
+    line =   ('------------------------------------------')
