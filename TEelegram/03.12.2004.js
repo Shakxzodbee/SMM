7206f81b48d777d3f47e2029c75bc0a51ef93fc9
Shakhzodbee <Bakhodirovichshakhzodbek@gmail.com>
2024-03-12 07:40:12 +0500

    Telegram movie pushshed

---
+++ TEelegram/03.12.2004.js
@@ -0,0 +1,14 @@
+let name = "Quiz app",
+    langues = {'Html' , 'Css' , 'Js'},
+    respons = '320px',
+    design  = 'no',
+    link    = 'https://jazzy-paprenjak-0675e2.netlify.app/',
+    langues2 = 'Uzbek'
+
+
+let telegramValue =  `    Name Quiz app⁉️🔄
+    langues 👩‍💻👩‍💻👩‍💻
+    responsive 👩‍💻320px
+    figma design 🚫
+    https://jazzy-paprenjak-0675e2.netlify.app/
+    langues 🇺🇿`
