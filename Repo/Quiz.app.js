84d23a22c0ef6bfeb91a7fe8af0c77968193afeb
Shakhzodbee <Bakhodirovichshakhzodbek@gmail.com>
2024-03-12 07:01:57 +0500

    Gituhb repo

---
+++ Repo/Quiz.app.js
@@ -0,0 +1,6 @@
+let name = "Quiz app",
+    langues = {'Html' , 'Css' , 'Js'},
+    respons = '320px',
+    design  = 'no',
+    link    = 'https://jazzy-paprenjak-0675e2.netlify.app/',
+    langues2 = 'Uzbek'
